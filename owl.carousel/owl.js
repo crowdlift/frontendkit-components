9a2aaf11669842e81f03e7e954bdd286ed0c76d8
Joe Johnston <joe@simple10.com>
2017-07-31 12:18:45 -0700

    Disable owl.carousel video plugin
    Causing build error due to image file

--- owl.carousel/owl.js
+++ owl.carousel/owl.js
@@ -3,7 +3,8 @@ import 'owl.carousel/src/js/owl.carousel';
 import 'owl.carousel/src/js/owl.autorefresh';
 import 'owl.carousel/src/js/owl.lazyload';
 import 'owl.carousel/src/js/owl.autoheight';
-import 'owl.carousel/src/js/owl.video';
+// Disable video since it references an image file that causes build errors
+// import 'owl.carousel/src/js/owl.video';
 import 'owl.carousel/src/js/owl.animate';
 import 'owl.carousel/src/js/owl.autoplay';
 import 'owl.carousel/src/js/owl.navigation';
