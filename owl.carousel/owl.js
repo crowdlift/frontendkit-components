b173c7e96b1fa78716d796f8a677bb8a0f98aaaf
Joe Johnston <joe@simple10.com>
2017-07-31 12:02:52 -0700

    Add owl.carousel v2.2.1

---
+++ owl.carousel/owl.js
@@ -0,0 +1,11 @@
+// Customize as needed
+import 'owl.carousel/src/js/owl.carousel';
+import 'owl.carousel/src/js/owl.autorefresh';
+import 'owl.carousel/src/js/owl.lazyload';
+import 'owl.carousel/src/js/owl.autoheight';
+import 'owl.carousel/src/js/owl.video';
+import 'owl.carousel/src/js/owl.animate';
+import 'owl.carousel/src/js/owl.autoplay';
+import 'owl.carousel/src/js/owl.navigation';
+import 'owl.carousel/src/js/owl.hash';
+import 'owl.carousel/src/js/owl.support';
