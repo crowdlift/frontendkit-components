a70c64c2bd57452c2f7a245ab80253d59383bce4
Joe Johnston <joe@simple10.com>
2017-09-08 13:08:56 -0700

    Improve mobile detection

--- helpers/ismobile.js
+++ helpers/ismobile.js
@@ -1,1 +1,3 @@
-export default (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i).test(navigator.userAgent);
+const isPhoneDevice = 'ontouchstart' in document.documentElement;
+const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i).test(navigator.userAgent);
+export default (isPhoneDevice || isMobile);
